217532812a54167a4f20e458f88e49a2c4da1643
AKIRA <2630938146@qq.com>
2025-12-01 17:07:56 +0800

    Update to merge two sorted lists

---
+++ zuoalg/CombineTwoListNode.cpp
@@ -0,0 +1,71 @@
+#include <cstddef>
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x):val(x),next(NULL){}
+};
+
+
+ListNode* CombineTwoList(ListNode* h1,ListNode*h2)
+{
+    if(h1==NULL && h2==NULL)    return NULL;
+        
+        //ListNode* head=(h1->val<=h2->val)? h1:h2;
+        ListNode* head;
+        ListNode*sub;
+        ListNode* main;
+        if(h1->val<=h2->val)
+        {
+             head=h1;
+             main=h1->next;
+             sub=h2;
+        }
+        else
+        {
+             head=h2;
+             main=h2->next;
+             sub=h1;
+
+        }
+         ListNode *pre=head;
+         ListNode* next=head->next;
+         h2=next;
+
+         while(sub!=NULL || main !=NULL)
+         {
+             if(sub==NULL)
+             {
+                 pre->next=main;
+                 break;
+             }
+             if(main==NULL)
+             {   
+                 pre->next=sub;
+                 break;
+             }
+             if(sub->val<=main->val)
+             {
+                 pre->next=sub;
+                 pre=sub;
+                 next=sub->next;
+                 sub=next;
+             }
+             else
+             {
+                 pre->next=main;
+                 pre=main;
+                 next=main->next;
+                 main=next;
+
+             }
+             
+         }
+         return head;
+
+}
+
+int main()
+{
+    
+}
